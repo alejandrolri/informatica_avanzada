fa48327a4ed6fd8bcfa3fd10972ef624f326c11e
Ruben176 <83211105+Ruben176@users.noreply.github.com>
2021-04-30 12:03:45 +0200

    Add files via upload

---
+++ monstruo.cpp
@@ -0,0 +1,16 @@
+#include "monstruo.h"
+
+Monstruo::Monstruo()
+{
+Monstruo Orco;
+Orco.Daño=10;
+Orco.Nivel=3;
+Monstruo Mago_del_abismo;
+Mago_del_abismo.Daño=30;
+Mago_del_abismo.Nivel=10;
+Monstruo Lilith;
+Lilith.Daño=100;
+Lilith.Nivel=50;
+
+}
+
