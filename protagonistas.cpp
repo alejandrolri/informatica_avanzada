87ab12216853ea8c53e20ff8cf1560d52809383d
alexLara97 <83211605+alexLara97@users.noreply.github.com>
2021-05-01 13:50:19 +0200

    Add files via upload

--- protagonistas.cpp
+++ protagonistas.cpp
@@ -18,41 +18,18 @@ void Protagonista::elegirRol(int){
 void Protagonista::vida(int){
 
 }
-void Protagonista::atacar(){
-}
-
-void Protagonista::avanzar(){
-
-}
-
-void Protagonista::huir(){
-
-}
-void Protagonista::informacionGeneral(){
-
-} 
 
 //Guerrero
 Guerrero::Guerrero(){
 }
-void Guerrero::equiparArma(){
+void Guerrero::AumentarFuerza(){
 
 }
-void Guerrero::equiparArmadura(){
-
-}
-
 //Mago
-void Mago::elegirGremio(){
-
-}
 void Mago::Magia(){
 
 }
 //Arquero
 void Arquero::equiparArco(){
-
-}
-void Arquero::seleccionarRaza(){
 
 }
